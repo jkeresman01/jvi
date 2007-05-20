f9d085d9da95728f4ae4f66366116b8f78299650
vdkuil <devnull@localhost>
2007-05-20 11:41:10 +0000

    clean up unused import

--- src/com/raelity/jvi/swing/DefaultViFactory.java
+++ src/com/raelity/jvi/swing/DefaultViFactory.java
@@ -32,7 +32,6 @@ package com.raelity.jvi.swing;
 import java.awt.Component;
 import java.awt.Container;
 import java.awt.KeyboardFocusManager;
-import java.awt.datatransfer.SystemFlavorMap;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyListener;
