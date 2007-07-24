5b39707b87b5a986366f856bb2eb8e9a1163d475
Ernie Rael <err@raelity.com>
2007-07-24 04:24:24 +0000

    All of the following are probably incomplete * Start ViTextView/ViBuffer cleanup/refactor   - Move document examination, for example getLineNumber, getSegment into     ViBuffer. Remove from ViTextView. Filesys/undo still need to be done.   - Move a bunch of textviewcache into Buffer; what remains is about     the JEditorPane and JScrollView.   - Mark is an inner class in Buffer   - use ViMark and ViFPOS everywhere possible. * Use generics and Override; get rid of redundant cast (but not NB yet). * Use char rather than int all over.

--- src/com/raelity/text/RegExpResultJava.java
+++ src/com/raelity/text/RegExpResultJava.java
@@ -11,33 +11,39 @@ public class RegExpResultJava extends RegExpResult
 	}
         this.result = m.toMatchResult();
     }
+    @Override
     public boolean isMatch() {
         return (null != result);
     }
+    @Override
     public int nGroup() {
         if(result == null) {
 	    return 0;
 	}
         return result.groupCount();
     }
+    @Override
     public String group(int i) {
         if(result == null) {
 	    return null;
 	}
         return result.group(i);
     }
+    @Override
     public int length(int i) {
         if(result == null || i > nGroup() || result.end(i) < 0) {
 	    return -1;
 	}
         return result.end(i) - result.start(i);
     }
+    @Override
     public int start(int i) {
         if(result == null) {
 	    return -1;
 	}
         return result.start(i);
     }
+    @Override
     public int stop(int i) {
         if(result == null) {
 	    return -1;
