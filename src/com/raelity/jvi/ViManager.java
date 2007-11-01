f8ba81656701cd1d0ba8142555ed2607f663b727
Ernie Rael <err@raelity.com>
2007-11-01 16:47:14 +0000

    - Post NB Beta2, NB changed spec version and package of the   indent module.

--- src/com/raelity/jvi/ViManager.java
+++ src/com/raelity/jvi/ViManager.java
@@ -101,7 +101,7 @@ public class ViManager {
   // 1.0.0.beta2 is NB vers 0.9.6.4
   // 1.0.0.beta3 is NB vers 0.9.7.5
   //
-  public static final jViVersion version = new jViVersion("1.1.1.beta1");
+  public static final jViVersion version = new jViVersion("1.1.2.x1");
   
   private static boolean enabled;
 
