4af9550aa0122ca95671406bc711228b1a1703e1
Ernie Rael <err@raelity.com>
2008-04-06 20:23:08 +0000

    - jVi-1.1 released. (1.1.4)

--- src/com/raelity/jvi/ViManager.java
+++ src/com/raelity/jvi/ViManager.java
@@ -106,7 +106,7 @@ public class ViManager {
   // 1.0.0.beta2 is NB vers 0.9.6.4
   // 1.0.0.beta3 is NB vers 0.9.7.5
   //
-  public static final jViVersion version = new jViVersion("1.1.4.x3");
+  public static final jViVersion version = new jViVersion("1.1.4");
   
   private static boolean enabled;
 
