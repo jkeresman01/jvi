2042a468916b09f254a7e0482471081ae21e8871
Ernie Rael <err@raelity.com>
2010-11-25 22:46:01 -0800

    bump micro rev

--- src/com/raelity/jvi/manager/ViManager.java
+++ src/com/raelity/jvi/manager/ViManager.java
@@ -75,7 +75,7 @@ public class ViManager
     // 1.0.0.beta2 is NB vers 0.9.6.4
     // 1.0.0.beta3 is NB vers 0.9.7.5
     //
-    public static final jViVersion version = new jViVersion("1.3.1.beta2.4");
+    public static final jViVersion version = new jViVersion("1.3.1.beta2.5");
 
     private static com.raelity.jvi.core.Hook core;
 
