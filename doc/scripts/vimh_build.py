6f1679ba3001ee886457c757a09404144f7df0f1
Ernie Rael <err@raelity.com>
2011-01-08 12:26:24 -0800

    Split Link handling into classes

--- doc/scripts/vimh_build.py
+++ doc/scripts/vimh_build.py
@@ -27,6 +27,13 @@ import vimh_scan as vs
 #       chars
 #       newline         col ignored
 
+def build_link_re_from_pat():
+    global RE_LINKWORD
+    RE_LINKWORD = re.compile(
+            vs.PAT_OPTWORD  + '|' + \
+            vs.PAT_CTRL     + '|' + \
+            vs.PAT_SPECIAL)
+
 #
 # This class is like a base class for accepting tokens
 # from VimHelpScanner. It simply collects what it gets, and can dump it.
@@ -55,6 +62,35 @@ class VimHelpBuildBase(object):
 
 RE_TAGLINE = re.compile(r'(\S+)\s+(\S+)')
 
+class Link:
+    def __init__(self, filename):
+        self.filename = filename
+
+class Links(dict):
+    def __missing__(self, key):
+        return None
+
+    def __init__(self, tags):
+        for line in tags:
+            m = RE_TAGLINE.match(line)
+            if m:
+                tag, filename = m.group(1, 2)
+                self.do_add_tag(filename, tag)
+
+    def do_add_tag(self, filename, tag):
+        # determine style for a plain link
+        style = 'hidden'
+        m = RE_LINKWORD.match(tag)
+        if m:
+            opt, ctrl, special = m.group('opt', 'ctrl', 'special')
+            if opt is not None: style = 'option'
+            elif ctrl is not None: style = 'keystroke'
+            elif special is not None: style = 'special'
+        link = Link(filename)
+        link.style = style
+        self[tag] = link
+
+
 #
 # XML builder
 #
@@ -63,64 +99,57 @@ RE_TAGLINE = re.compile(r'(\S+)\s+(\S+)')
 # Simple Html builder, should reproduce original work from Carlo
 #
 
-def build_html_re_from_pat():
-    global RE_LINKWORD
-    RE_LINKWORD = re.compile(
-            vs.PAT_OPTWORD  + '|' + \
-            vs.PAT_CTRL     + '|' + \
-            vs.PAT_SPECIAL)
-
-class HtmlLink:
-    def __init__(self, link_pipe, link_plain):
-        self.link_pipe = link_pipe
-        self.link_plain = link_plain
-
-class VimHelpBuildHtml(VimHelpBuildBase):
-    urls = { }
+# Note that the lazy creation of link_plain,link_pipe 
+# provides for reporting defined links that are not referenced
+class HtmlLinks(Links):
+    # styles map to the html style class for the link
+    styles = dict(link='l', option='o', keystroke='k',
+                  special='s', hidden='d')
 
     def __init__(self, tags):
-        build_html_re_from_pat()
-        for line in tags:
-            m = RE_TAGLINE.match(line)
-            if m:
-                tag, filename = m.group(1, 2)
-                self.do_add_tag(filename, tag)
+        super(HtmlLinks, self).__init__(tags)
 
-    def add_tags(self, filename, contents):
-        for match in RE_STARTAG.finditer(contents):
-            tag = match.group(1).replace('\\', '\\\\').replace('/', '\\/')
-            self.do_add_tag(filename, tag)
-
-    def do_add_tag(self, filename, tag):
-        part1 = '<a href="' + filename + '.html#' + \
-                urllib.quote_plus(tag) + '"'
-        part2 = '>' + cgi.escape(tag) + '</a>'
-        link_pipe = part1 + ' class="l"' + part2
-        classattr = ' class="d"'
-        m = RE_LINKWORD.match(tag)
-        if m:
-            opt, ctrl, special = m.group('opt', 'ctrl', 'special')
-            if opt is not None: classattr = ' class="o"'
-            elif ctrl is not None: classattr = ' class="k"'
-            elif special is not None: classattr = ' class="s"'
-        link_plain = part1 + classattr + part2
-        self.urls[tag] = HtmlLink(link_pipe, link_plain)
+    def get_tag(self, tag):
+        """Lazily create link_plain and link_pipe."""
+        link = self[tag]
+        if not link : return None
+        if not hasattr(link, 'link_plain'):
+            part1 = '<a href="' + link.filename + '.html#' + \
+                    urllib.quote_plus(tag) + '"'
+            part2 = '>' + cgi.escape(tag) + '</a>'
+            link.link_pipe = part1 \
+                    + ' class="' + self.styles['link'] + '"' + part2
+            link.link_plain = part1 \
+                    + ' class="' + self.styles[link.style] + '"' + part2
+        return link
 
     def maplink(self, tag, css_class = None):
-        links = self.urls.get(tag)
-        if links is not None:
-            if css_class == 'l':
+        link = self.get_tag(tag)
+        if link is not None:
+            # this is a known link from the tags file
+            if css_class == 'link':
                 # drop the anchor if foo.txt and foo.txt.html#foo.txt
                 if tag.endswith('.txt') \
-                        and links.link_pipe.find(
+                        and link.link_pipe.find(
                                 '"' + tag + '.html#' + tag + '"') >= 0:
-                    return links.link_pipe.replace('#' + tag, '');
-                return links.link_pipe
-            else: return links.link_plain
+                    return link.link_pipe.replace('#' + tag, '');
+                return link.link_pipe
+            else: return link.link_plain
         elif css_class is not None:
-            return '<span class="' + css_class + '">' + cgi.escape(tag) + \
-                    '</span>'
-        else: return cgi.escape(tag)
+            # not a known link, but a class was specified
+            return '<span class="' + self.styles[css_class] \
+                    + '">' + cgi.escape(tag) + '</span>'
+        else:
+            # not know link, no class specifed, just return it
+            return cgi.escape(tag)
+
+
+class VimHelpBuildHtml(VimHelpBuildBase):
+
+    def __init__(self, tags):
+        build_link_re_from_pat()
+        self.links = HtmlLinks(tags)
+
 
     def markup(self, markup):
         markup = markup.strip()
@@ -133,17 +162,17 @@ class VimHelpBuildHtml(VimHelpBuildBase):
         token, chars, col = token_data
         #print token_data
         if 'pipe' == token:
-            self.out.append(self.maplink(chars, 'l'))
+            self.out.append(self.links.maplink(chars, 'link'))
         elif 'star' == token:
             tag = chars
             self.out.append('<a name="' + urllib.quote_plus(tag) +
                     '" class="t">' + cgi.escape(tag) + '</a>')
         elif 'opt' == token:
-            self.out.append(self.maplink(chars, 'o'))
+            self.out.append(self.links.maplink(chars, 'option'))
         elif 'ctrl' == token:
-            self.out.append(self.maplink(chars, 'k'))
+            self.out.append(self.links.maplink(chars, 'keystroke'))
         elif 'special' == token:
-            self.out.append(self.maplink(chars, 's'))
+            self.out.append(self.links.maplink(chars, 'special'))
         elif 'title' == token:
             self.out.append('<span class="i">' +
                     cgi.escape(chars) + '</span>')
@@ -161,7 +190,7 @@ class VimHelpBuildHtml(VimHelpBuildBase):
             self.out.append('<a class="u" href="' + chars + '">' +
                     cgi.escape(chars) + '</a>')
         elif 'word' == token:
-            self.out.append(self.maplink(chars))
+            self.out.append(self.links.maplink(chars))
         elif 'example' == token:
             self.out.append('<span class="e">' + cgi.escape(chars) +
                     '</span>\n')
