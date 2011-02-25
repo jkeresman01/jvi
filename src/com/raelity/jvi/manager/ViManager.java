d24f6bc5241c2330c0e97b79a72c3c6d1e130f5e
Ernie Rael <err@raelity.com>
2011-02-25 15:14:55 -0800

    Bump micro rev - nbvi-1.3.1.beta3.4

--- src/com/raelity/jvi/manager/ViManager.java
+++ src/com/raelity/jvi/manager/ViManager.java
@@ -78,7 +78,7 @@ public class ViManager
     // 1.0.0.beta3 is NB vers 0.9.7.5
     //
     // 1.3.1.beta3 is module rev 1.3.0 
-    public static final jViVersion version = new jViVersion("1.3.1.beta3.3");
+    public static final jViVersion version = new jViVersion("1.3.1.beta3.4");
 
     private static com.raelity.jvi.core.Hook core;
 
