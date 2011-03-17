af579307586e0b2b140f75d11b93349190d5b244
Ernie Rael <err@raelity.com>
2011-03-17 15:15:07 -0700

    Create class CharTab and test. Will use with 'iskeyword' option.

---
+++ src/com/raelity/jvi/lib/CharTab.java
@@ -0,0 +1,185 @@
+/*
+ * The contents of this file are subject to the Mozilla Public
+ * License Version 1.1 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of
+ * the License at http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS
+ * IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * rights and limitations under the License.
+ *
+ * The Original Code is jvi - vi editor clone.
+ *
+ * The Initial Developer of the Original Code is Ernie Rael.
+ * Portions created by Ernie Rael are
+ * Copyright (C) 2000-2010 Ernie Rael.  All Rights Reserved.
+ *
+ * Contributor(s): Ernie Rael <err@raelity.com>
+ */
+
+package com.raelity.jvi.lib;
+
+import java.util.BitSet;
+
+/**
+ * CharTab implements the vim 'iskeyword' related behavior.
+ *
+ * @author Ernie Rael <err at raelity.com>
+ */
+public class CharTab implements Cloneable {
+
+    private BitSet table = new BitSet(256);
+
+    /**
+     * Parse the spec.
+     * Do iskeyword processing only
+     */
+    public boolean init(String spec) {
+        table.clear();
+
+        int p = 0;
+        char c;
+        int c2;
+        boolean tilde;
+        boolean do_isalpha;
+        MutableInt mi = new MutableInt();
+
+	while (p < spec.length())
+	{
+	    tilde = false;
+	    do_isalpha = false;
+	    if (spec.charAt(p) == '^' && p < (spec.length() - 1))
+	    {
+		tilde = true;
+		++p;
+	    }
+            mi.setValue(p);
+            c = getCharValue(spec, mi);
+            p = mi.getValue();
+	    c2 = -1;
+	    if (spec.charAt(p) == '-' && p < (spec.length() - 1))
+	    {
+		++p;
+                mi.setValue(p);
+                c2 = getCharValue(spec, mi);
+                p = mi.getValue();
+	    }
+	    if (c <= 0 || (c2 < c && c2 != -1) || c2 >= 256
+                     || !(p == spec.length() || spec.charAt(p) == ','))
+		return false;
+
+	    if (c2 == -1)	/* not a range */
+	    {
+		// A single '@' (not "@-@"):
+		// Decide on letters being ID/printable/keyword chars with
+		// standard function isalpha(). This takes care of locale for
+		// single-byte characters).
+		if (c == '@')
+		{
+		    do_isalpha = true;
+		    c = 1;
+		    c2 = 255;
+		}
+		else
+		    c2 = c;
+	    }
+	    while (c <= c2)
+	    {
+		if (!do_isalpha || isalpha(c)
+// #ifdef FEAT_FKMAP
+// 			|| (p_altkeymap && (F_isalpha(c) || F_isdigit(c)))
+// #endif
+			    )
+		{
+		    /* (re)set keyword flag */
+                    if (tilde)
+                        table.clear(c);
+                    else
+                        table.set(c);
+		}
+		++c;
+	    }
+	    p = skip_to_option_part(spec, p);
+	}
+        return true;
+    }
+
+    public boolean iswordc(char c) {
+        boolean isWord;
+        if(c < 0x100) {
+            isWord = table.get(c);
+        } else {
+            isWord = Character.isLetterOrDigit(c);
+        }
+        return isWord;
+    }
+
+    private BitSet getTableClone() {
+        return (BitSet)table.clone();
+    }
+
+    @Override
+    public CharTab clone() {
+        CharTab o = null;
+        try {
+            o = (CharTab)super.clone();
+            o.table = (BitSet)table.clone();
+        } catch(CloneNotSupportedException ex) {
+        }
+        return o;
+    }
+
+    private static boolean isdigit(char c) {
+        return c >= '0' && c <= '9';
+    }
+
+    private static boolean isalpha(char c) {
+        return c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z';
+    }
+
+    private static int skip_to_option_part(String s, int p) {
+        if(p >= s.length())
+            return p;
+        if(s.charAt(p) == ',')
+            p++;
+        while(p < s.length() && s.charAt(p) == ' ')
+            p++;
+        return p;
+    }
+
+    private static char getdigits(String s, MutableInt mi) {
+        int p = mi.getValue();
+        int q = p;
+
+        while(q < s.length() && isdigit(s.charAt(q)))
+            q++;
+        mi.setValue(q);
+
+        int i = 0;
+        try {
+            i = Integer.parseInt(s.substring(p, q));
+        } catch(NumberFormatException ex) {
+        }
+        if(i >= 0x100)
+            i = 0;
+        
+        return (char)i;
+    }
+
+    private char getCharValue(String s, MutableInt mi) {
+        int p = mi.getValue();
+        char c = 0;
+
+        if (isdigit(s.charAt(p))) {
+            c = getdigits(s, mi);
+        } else {
+            c = s.charAt(p);
+            p++;
+            mi.setValue(p);
+        }
+
+        return c;
+    }
+
+}
---
+++ test/com/raelity/jvi/lib/CharTabTest.java
@@ -0,0 +1,175 @@
+/*
+ * The contents of this file are subject to the Mozilla Public
+ * License Version 1.1 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of
+ * the License at http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS
+ * IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * rights and limitations under the License.
+ *
+ * The Original Code is jvi - vi editor clone.
+ *
+ * The Initial Developer of the Original Code is Ernie Rael.
+ * Portions created by Ernie Rael are
+ * Copyright (C) 2000-2010 Ernie Rael.  All Rights Reserved.
+ *
+ * Contributor(s): Ernie Rael <err@raelity.com>
+ */
+
+package com.raelity.jvi.lib;
+
+import java.util.BitSet;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Ernie Rael <err at raelity.com>
+ */
+public class CharTabTest {
+
+    public CharTabTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() throws Exception
+    {
+        // setup the default IsWordC
+    }
+    
+    @AfterClass
+    public static void tearDownClass() throws Exception
+    {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    // This interface provides the 
+    // pattern spec and the expected values
+    interface IsWordC {
+        String getSpec();
+        boolean iswordc(char c);
+    }
+
+    static class DefaultIsWordC implements IsWordC
+    {
+        @Override public String getSpec()
+        { return "@,48-57,_,192-255"; }
+
+        @Override
+        public boolean iswordc(char c)
+        {
+            boolean isWord =
+                'a' <= c && c <= 'z'
+                || 'A' <= c && c <= 'Z'
+                || 48 <= c && c <= 57 // '0'...'9'
+                || '_' == c
+                || 192 <= c && c <= 255
+                ;
+            return isWord;
+        }
+
+    }
+
+    static class NoAlphaIsWordC implements IsWordC
+    {
+        @Override
+        public String getSpec()
+        {
+            return "48-57,_,192-255";
+        }
+
+        @Override
+        public boolean iswordc(char c)
+        {
+            boolean isWord =
+                48 <= c && c <= 57 // '0'...'9'
+                || '_' == c
+                || 192 <= c && c <= 255
+                ;
+            return isWord;
+        }
+
+    }
+
+    static class ExcludeIsWordC implements IsWordC
+    {
+        @Override
+        public String getSpec()
+        {
+            return "48-57,^50-52,_,192-255";
+        }
+
+        @Override
+        public boolean iswordc(char c)
+        {
+            if(50 <= c && c <= 52)
+                return false;
+            boolean isWord =
+                48 <= c && c <= 57 // '0'...'9'
+                || '_' == c
+                || 192 <= c && c <= 255
+                ;
+            return isWord;
+        }
+
+    }
+
+    byte[] asArray(IsWordC isWordC) {
+        byte[] b = new byte[256];
+        for(char c = 0; c < 256; c++) {
+            b[c] = isWordC.iswordc(c) ? (byte)1 : (byte)0;
+        }
+        return b;
+    }
+
+    byte[] asArray(CharTab ct) {
+        byte[] b = new byte[256];
+        for(char c = 0; c < 256; c++)
+            b[c] = ct.iswordc(c) ? (byte)1 : (byte)0;
+        return b;
+    }
+
+    void runTest(IsWordC t) {
+        System.out.println("\t" + t.getSpec());
+        byte[] expResult = asArray(t);
+        CharTab ct = new CharTab();
+        ct.init(t.getSpec());
+        byte[] result = asArray(ct);
+        assertArrayEquals(t.getSpec(), expResult, result);
+    }
+    
+    @Test
+    public void testIswordc()
+    {
+        System.out.println("iswordc");
+        runTest(new DefaultIsWordC());
+        runTest(new NoAlphaIsWordC());
+        runTest(new ExcludeIsWordC());
+    }
+    
+    // @Test
+    public void testClone()
+    {
+        System.out.println("clone");
+        CharTab instance = new CharTab();
+        CharTab expResult = null;
+        CharTab result = instance.clone();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+}
