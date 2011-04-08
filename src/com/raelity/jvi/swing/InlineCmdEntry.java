5388786f3608ec2f181ff9e7ac675211f95bc100
Ernie Rael <err@raelity.com>
2011-04-08 09:09:49 -0700

    Fixup some coments

--- src/com/raelity/jvi/swing/InlineCmdEntry.java
+++ src/com/raelity/jvi/swing/InlineCmdEntry.java
@@ -1,8 +1,21 @@
 /*
- * A command line entry widget that sits on the glass pane,
- * instead of a modal dialog; the modal dialog is preferred.
- * Can avoid problems with interpreter bugs, particularly
- * on early linux interpreters, around 2002.
+ * The contents of this file are subject to the Mozilla Public
+ * License Version 1.1 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of
+ * the License at http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS
+ * IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * rights and limitations under the License.
+ *
+ * The Original Code is jvi - vi editor clone.
+ *
+ * The Initial Developer of the Original Code is Ernie Rael.
+ * Portions created by Ernie Rael are
+ * Copyright (C) 2000 Ernie Rael.  All Rights Reserved.
+ *
+ * Contributor(s): Ernie Rael <err@raelity.com>
  */
 
 package com.raelity.jvi.swing;
@@ -11,6 +24,12 @@ import java.awt.event.*;
 import javax.swing.*;
 import java.awt.*;
 
+/**
+ * A command line entry widget that sits on the glass pane,
+ * instead of a modal dialog; the modal dialog is preferred.
+ * This widget can avoid problems with interpreter bugs,
+ * particularly on early linux interpreters, around 2002.
+ */
 public class InlineCmdEntry extends CommandLine.CommandLineEntry {
     private MouseListener mouseListener;
     private boolean doneWithCommandLine;
