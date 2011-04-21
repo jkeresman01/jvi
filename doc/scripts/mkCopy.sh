ddba05f2e40514295dcc8e605144e9fee541fc9d
Ernie Rael <err@raelity.com>
2011-04-21 16:26:00 -0700

    Include the javahelpset in htdocs

--- doc/scripts/mkCopy.sh
+++ doc/scripts/mkCopy.sh
@@ -2,11 +2,17 @@
 
 . VARS.sh
 
+set -e
 
 OUT=$HTDOCS_OUT
 
 mkdir -p $OUT
 cp -pr $HTDOCS/* $OUT
+
+rm $OUT/javahelpset
+ln -s $jvi/nbvi/javahelp/org/netbeans/modules/jvi/docs \
+      $OUT/javahelpset
+
 mkdir -p $OUT/vimhelp
 cp -p $VIMHELP_OUT/* $OUT/vimhelp
 
