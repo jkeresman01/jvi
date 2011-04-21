da4d55a067c73647efdcb470e3bec35be568966b
Ernie Rael <err@raelity.com>
2011-04-20 19:03:06 -0700

    The common vars file

---
+++ doc/scripts/VARS.sh
@@ -0,0 +1,16 @@
+
+# This is '.' by the document build stuff
+
+BASE=..
+
+SCRIPTS=$BASE/scripts
+HTDOCS=$BASE/htdocs
+UNFILTERED=$BASE/vimhelp
+VIM_CSS=$BASE/css
+
+FILTERED=$BASE/build/filtered
+
+VIMHELP_OUT=$BASE/build/txt_html
+HTDOCS_OUT=$BASE/build/htdocs
+
+set -e
