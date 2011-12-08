4b10ae509d326bc0f04a6c54b8b13bd642f03226
Ernie Rael <err@raelity.com>
2011-12-08 15:51:40 -0800

    Disable jVi on NB-71; see NetBeans Bug 205835.

--- src/com/raelity/jvi/manager/ViManager.java
+++ src/com/raelity/jvi/manager/ViManager.java
@@ -82,7 +82,7 @@ public class ViManager
     // 1.4.0 is module rev 1.4.9
     // 1.4.1.x2 is module rev 1.4.12
     //
-    public static final jViVersion version = new jViVersion("1.4.5.rc2");
+    public static final jViVersion version = new jViVersion("1.4.5.rc3");
 
     private static com.raelity.jvi.core.Hook core;
 
