27f94368c567327227540e9037660aa35b574a60
Ernie Rael <err@raelity.com>
2013-08-13 09:19:43 -0700

    Stop putting out the nb-70 patches.

--- doc/scripts/mkREL.sh
+++ doc/scripts/mkREL.sh
@@ -30,8 +30,6 @@ JVI_ADD="
     $JVI_RELDIR/editor.pin/editor.pin-1.3.2/proj/updates.xml
     $JVI_RELDIR/jvi.help/jvi.help-1.0/proj/updates.xml
     $JVI_RELDIR/nb-jvi-spi/nb-jvi-spi-1.5/proj/updates.xml
-    $JVI_RELDIR/nb-701/openide.text-1.0/proj/updates.xml
-    $JVI_RELDIR/nb-701/editor.completion-1.0/proj/updates.xml
     "
 
 OUT=$JVI_VERSIONDIR/build-uc
