594cc5044cbee1d22e8fe8062d34de4e4fc34479
Ernie Rael <err@raelity.com>
2013-11-10 17:47:06 -0800

    1.5.1.x2 (new SPI/:tog) jdk1-6

--- doc/scripts/mkREL.sh
+++ doc/scripts/mkREL.sh
@@ -29,7 +29,7 @@ UC_DIR=UC
 JVI_ADD="
     $JVI_RELDIR/editor.pin/editor.pin-1.3.2/proj/updates.xml
     $JVI_RELDIR/jvi.help/jvi.help-1.0/proj/updates.xml
-    $JVI_RELDIR/nb-jvi-spi/nb-jvi-spi-1.5/proj/updates.xml
+    $JVI_RELDIR/nb-jvi-spi/nb-jvi-spi-1.6/proj/updates.xml
     "
 
 OUT=$JVI_VERSIONDIR/build-uc
--- src/com/raelity/jvi/manager/ViManager.java
+++ src/com/raelity/jvi/manager/ViManager.java
@@ -84,7 +84,7 @@ public class ViManager
     // 1.4.0 is module rev 1.4.9
     // 1.4.1.x2 is module rev 1.4.12
     //
-    public static final jViVersion version = new jViVersion("1.5.1.x1");
+    public static final jViVersion version = new jViVersion("1.5.1.x2");
 
     private static com.raelity.jvi.core.Hook core;
 
