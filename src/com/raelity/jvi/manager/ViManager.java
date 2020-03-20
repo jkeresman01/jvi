a23b215dc1da89419aa664db6f5cb701e91d126f
Ernie Rael <errael@raelity.com>
2020-03-20 23:35:51 +0100

    bumps for 1.6.1

--- src/com/raelity/jvi/manager/ViManager.java
+++ src/com/raelity/jvi/manager/ViManager.java
@@ -86,7 +86,7 @@ final public class ViManager
     // 1.4.0 is module rev 1.4.9
     // 1.4.1.x2 is module rev 1.4.12
     //
-    public static final jViVersion version = new jViVersion("1.6.1.beta3");
+    public static final jViVersion version = new jViVersion("1.6.1");
 
     private static com.raelity.jvi.core.Hook core;
 
